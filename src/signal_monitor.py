ab2307326800485056db103f98fdcdf633485ee1
Hordur Heidarsson <heidarss@usc.edu>
2016-01-28 14:05:03 -0800

    Added publishing when no observations are received to signal monitor

--- src/signal_monitor.py
+++ src/signal_monitor.py
@@ -9,6 +9,8 @@ from piksi_ros.msg import Observations, Obs, Ephemeris, SignalStatus
 
 import numpy as np
 
+import time
+
 def reject_outliers(data, m = 2.):
     d = np.abs(data - np.median(data))
     mdev = np.median(d)
@@ -20,10 +22,14 @@ class SignalMonitor():
     def __init__(self):
         rospy.init_node("piksi_signal_monitor")
 
+        self.last_obs_time = 0
+        self.obs_timeout = 0.75
+
         self.signal_pub = rospy.Publisher("~signal_status", SignalStatus, queue_size=1000)
         self.obs_sub = rospy.Subscriber("observations", Observations, callback=self.obs_callback)
 
     def obs_callback(self, msg):
+        self.last_obs_time = time.time()
         cn0 = np.array([obs.cn0/4.0 for obs in msg.obs])
 
         m = SignalStatus()
@@ -34,9 +40,24 @@ class SignalMonitor():
         m.num_sats = len(msg.obs)
         self.signal_pub.publish(m)
 
+    def send_zero_status(self):
+        m = SignalStatus()
+        m.header.stamp = rospy.Time.now()
+        m.mean_cn0 = 0
+        m.median_cn0 = 0
+        m.robust_mean_cn0 = 0
+        m.num_sats = 0
+        self.signal_pub.publish(m)
+
+    def spin(self):
+        while not rospy.is_shutdown():
+            if time.time() - self.last_obs_time > self.obs_timeout:
+                self.send_zero_status()
+            rospy.sleep(1.0)
+
 def main():
     node = SignalMonitor()
-    rospy.spin()
+    node.spin()
 
 if __name__ == "__main__":
     main()
